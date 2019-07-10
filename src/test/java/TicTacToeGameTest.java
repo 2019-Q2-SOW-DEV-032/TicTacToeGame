83b48b543c189983be203cfc5c700c83730f89c1
2019-Q2_SOW_DEV_032 <sansindhu14@gmail.com>
2019-07-10 14:02:31 +0200

    Refactored by removing the duplicated lines of code in test class

--- src/test/java/TicTacToeGameTest.java
+++ src/test/java/TicTacToeGameTest.java
@@ -1,19 +1,24 @@
 package TicTacToe;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TicTacToeGameTest {
+    private TicTacToeGame ticTacToeGame;
+
+    @Before
+    public void setUp() throws Exception{
+        ticTacToeGame = new TicTacToeGame();
+    }
 
     @Test
     public void test_checkIfPlayerXPlaceFirstMove(){
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         assertEquals('X', ticTacToeGame.getCurrentPlayerMark());
     }
 
     @Test
     public void test_WhenFirstPlayerIsX_thenNextPlayerIsO(){
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         ticTacToeGame.changePlayer();
         assertEquals('O',ticTacToeGame.getCurrentPlayerMark());
     }
